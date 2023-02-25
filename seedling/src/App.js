22b47fae1bbe9330965d1f918794e9d4c18437b5
Hong Sheng <lee_hong_sheng@hotmail.com>
2023-02-25 11:54:26 +0800

    Fix repeated imports in App.js

--- seedling/src/App.js
+++ seedling/src/App.js
@@ -3,12 +3,6 @@ import LoginPage from './pages/LoginPage';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import { EditClaim } from './components/Edit/Edit';
-import { 
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Navigate
- } from 'react-router-dom'
 
 function App() {
   return (
