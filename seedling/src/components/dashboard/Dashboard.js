d8798c69312178f476ed01e19ab414af4ded607a
Aliciaho <E0425365@u.nus.edu>
2023-02-25 11:57:06 +0800

    feat: add table to dashboard

--- seedling/src/components/dashboard/Dashboard.js
+++ seedling/src/components/dashboard/Dashboard.js
@@ -1,12 +1,25 @@
 import React from 'react';
+import {Grid, Box, Button, Typography} from '@mui/material';
+import BasicTable from './BasicTable';
 
 function DashboardContent() {
     return(
-        <div>
-            <h1>
-                Hello!
-            </h1>
-        </div>
+        <Box sx={{margin: 2}}>
+            <Grid container spacing={2} justifyContent="space-between" sx={{mb: 5}}>
+                <Grid item>
+                   <Typography variant="h4">View Insurance Claims</Typography>
+                </Grid>
+                <Grid item>
+                    <Button variant="outlined" sx={{mr: 2}}>
+                        Logout
+                    </Button>
+                    <Button variant="contained">
+                        Add
+                    </Button>
+                </Grid>
+            </Grid>
+            <BasicTable />
+        </Box>  
     );
 }
 
