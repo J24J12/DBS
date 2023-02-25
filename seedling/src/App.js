5fd289f41621f5586a274639ed498fc96f3310ef
AtlasWongy <atlasyjyj@gmail.com>
2023-02-25 11:37:14 +0800

    Added Routes

--- seedling/src/App.js
+++ seedling/src/App.js
@@ -1,11 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 import { EditClaim } from './components/Edit/Edit';
+import { 
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate
+ } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      <EditClaim/>
+      <Router>
+        <Routes>
+          <Route exact path="/edit" element={<EditClaim/>}></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
